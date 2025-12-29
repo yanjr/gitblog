92f32096308da607b0e260dd60a574a07a848b04
严佳瑞 <yanjr@users.noreply.github.com>
2025-12-29 12:34:03 +0800

    Update link for 'Things I like' section

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from marko.ext.gfm import gfm as marko
 
 MD_HEAD = """## [Gitblog](https://yanjr.github.io/gitblog/)
 My personal blog([About Me](https://github.com/yanjr/gitblog/issues/12)) using issues and GitHub Actions (随意转载，无需署名)
-[Things I like](https://github.com/yanjr/gitblog/issues/311)
+[Things I like](https://github.com/yanjr/gitblog/issues/13)
 ![image](https://github.com/user-attachments/assets/a168bf11-661e-4566-b042-7fc9544de528)
 [RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
 """
